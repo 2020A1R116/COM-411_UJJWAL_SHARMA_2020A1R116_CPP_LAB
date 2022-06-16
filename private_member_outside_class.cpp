d42034a08309ec0dec5dc23b7c638d541dc2742b
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2022-06-16 21:34:29 +0530

    ACCESS PRIVATE MEMBER FUNCTION OUTSIDE A CLASS WITHOUT A FRIEND

---
+++ private_member_outside_class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+private:
+    int x;
+    int y;
+};
+
+// Driver Code
+int main()
+{
+    A a;
+    int *p = (int *)&a;
+    *p = 3;
+    p++;
+    *p = 9;
+    p--;
+    cout << endl
+         << "x = " << *p;
+    p++;
+    cout << endl
+         << "y = " << *p;
+    return 0;
+}
